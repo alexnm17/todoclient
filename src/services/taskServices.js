45f49a806c27ed00abeb440c13d0678cd4da900a
Diego Sepúlveda Millán <92218221+DiegoSM1998@users.noreply.github.com>
2022-04-08 12:01:39 +0200

    icons and solve default priority task
    icons and solve default priority task

---
+++ src/services/taskServices.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+const backendUrl = "http://localhost:4000/tasks";
+
+export function addTask(task) {
+    return axios.post(backendUrl, task);
+}
+
+export function getTasks() {
+    return axios.get(backendUrl);
+}
+
+export function getTask(task_id){
+    return axios.get(backendUrl+ "/" + task_id)
+}
+
+export function updateTask(task_id, task_info) {
+    return axios.put(backendUrl+ "/" + task_id, task_info);
+}
+
+export function deleteTask(task_id) {
+    return axios.delete(backendUrl+ "/" + task_id);
+}
