783b1301389e5246f6355937f6314b01137fc567
Diego Sepúlveda Millán <92218221+DiegoSM1998@users.noreply.github.com>
2022-04-26 01:27:16 +0200

    try authToken
    No funciona

---
+++ src/components/autenticacion.js
@@ -0,0 +1,21 @@
+
+const jwt = require("jose")
+
+let verificarToken = (req, res, next) => {
+  let token = req.get("Authorization")
+
+  jwt.verify(token, "secret", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err
+      })
+    }
+    req.usuariobd=decoded.usuariobd
+    next()
+  })
+}
+
+module.exports = {
+  verificarToken
+}
